571ece3ea2e0e4a112956c8c791bd9a155406744
Dipayan Bose <76175101+bosedipayan@users.noreply.github.com>
2026-05-26 08:59:18 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+
+        for(auto it: s)
+        {
+            mp[it]++;
+        }
+
+        for(auto it: t)
+        {
+            if(mp.find(it) != mp.end())
+            {
+                mp[it]--;
+
+                if(mp[it] == 0)
+                {
+                    mp.erase(it);
+                }
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        return (mp.size() == 0);
+    }
+};
