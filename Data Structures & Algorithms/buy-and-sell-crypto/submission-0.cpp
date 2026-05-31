7c4954d57214566ce86d0d9ad5e396d201d12591
Dipayan Bose <76175101+bosedipayan@users.noreply.github.com>
2026-05-31 22:24:04 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int miniPrice = prices[0];
+
+        int ans=INT_MIN;
+        for(int i=1;i<n;i++)
+        {
+            miniPrice=min(miniPrice, prices[i]); // 1
+            ans=max(ans, abs(prices[i] - miniPrice));
+        }
+
+        return ans;
+    }
+};
